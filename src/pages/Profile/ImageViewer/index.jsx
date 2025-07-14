ccfee3c65aab2d864d0ff11d7a4993c0e6d291e0
minhzom <minh1382005@gmail.com>
2025-07-14 16:27:30 +0700

    thêm chức năng thay ảnh đại diện cho user

---
+++ src/pages/Profile/ImageViewer/index.jsx
@@ -0,0 +1,76 @@
+import { useRef } from "react";
+import "./ImageViewer.scss";
+import { X } from "lucide-react";
+import axios from "axios";
+
+function ImageViewer({ isOwner, images, onClose }) {
+  const fileInputRef = useRef();
+  const API_BASE_URL = "http://localhost:8080";
+
+  const handlingAddAvatar = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("avatar", file);
+
+    const token = localStorage.getItem("token");
+
+    try {
+      axios
+        .post(`${API_BASE_URL}/api/profiles/update-avatar`, formData, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(() => {
+          alert("đã thay thành công ảnh đại diện");
+          onClose();
+
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+        })
+        .catch(() => {
+          alert("lỗi chưa thể thay được ảnh đại diện");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="image-viewer">
+      <div className="box-overlay" onClick={() => onClose()}></div>
+      <div className="image-viewer__content">
+        <button className="close-btn" onClick={onClose}>
+          <X size={30} />
+        </button>
+
+        <img src={images} alt="avt" />
+        {isOwner && (
+          <div>
+            <button
+              className="add-avatar"
+              onClick={() => fileInputRef.current?.click()}
+            >
+              Thêm mới avatar
+            </button>
+
+            <input
+              type="file"
+              ref={fileInputRef}
+              accept="image/*"
+              style={{ display: "none" }}
+              onChange={handlingAddAvatar}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ImageViewer;
--- src/pages/Profile/ProfileInfo/index.jsx
+++ src/pages/Profile/ProfileInfo/index.jsx
@@ -1,70 +1,84 @@
 import { useState } from "react";
 import "./ProfileInfo.scss";
 import { NavLink, useNavigate } from "react-router-dom";
+import ImageViewer from "../ImageViewer";
 
 function ProfileInfo({ userInfo, isOwner, handlingOpenEditProfileComponent }) {
-  const [addFriend, setAddFriend] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
-  const navigate = useNavigate();
+    const [addFriend, setAddFriend] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
+    const navigate = useNavigate();
+    const [showImageViewer, setShowImageViewer] = useState(false);
 
-  return (
-    <div className="profile-info">
-        <div className="container">
-            <div className="profile-content">
-                <div className="profile-avatar">
-                    <img src={userInfo.avatarUrl} alt="Profile" />
-                </div>
-                
-                <div className="profile-details">
-                    <div className="profile-header-info">
-                        <h2>{userInfo.fullName}</h2>
+    const handlingViewAvatar = () => {
+        setShowImageViewer(true);
+    }
 
-                        <div className="profile-actions">
-                            {!isOwner && (
-                            <>
-                                <button
-                                    className={`add-friend-btn ${userInfo.role == "FRIEND" ? 'is-friend' : ''}`}
-                                    onMouseEnter={() => setIsHovered(true)}
-                                    onMouseLeave={() => setIsHovered(false)}
-                                >
-                                     {userInfo.role === "FRIEND"
-                                        ? isHovered
-                                            ? "Hủy kết bạn"
-                                            : "Bạn bè"
-                                        : "Gửi kết bạn"}
+    return (
+        <div className="profile-info">
+            <div className="container">
+                <div className="profile-content">
+                    <div className="profile-avatar">
+                        <img src={userInfo.avatarUrl} alt="Profile" onClick={handlingViewAvatar}/>
+                    </div>
+                    
+                    <div className="profile-details">
+                        <div className="profile-header-info">
+                            <h2>{userInfo.fullName}</h2>
 
-                                </button>
-                                <button className="message-btn" onClick={() => {navigate(`/messages/${userInfo.id}`)}}>Nhắn tin</button>
-                            </>
-                            )}
-                            
-                            {/* btn chỉnh sửa profile cho mình */}
-                            {isOwner && (
-                                <button 
-                                    className="edit-profile-btn" 
-                                    onClick={() => handlingOpenEditProfileComponent()}>
-                                        Chỉnh sửa trang cá nhân
-                                </button>
-                            )}
-                            
-                            <button className="more-btn">⋯</button>
+                            <div className="profile-actions">
+                                {!isOwner && (
+                                <>
+                                    <button
+                                        className={`add-friend-btn ${userInfo.role == "FRIEND" ? 'is-friend' : ''}`}
+                                        onMouseEnter={() => setIsHovered(true)}
+                                        onMouseLeave={() => setIsHovered(false)}
+                                    >
+                                        {userInfo.role === "FRIEND"
+                                            ? isHovered
+                                                ? "Hủy kết bạn"
+                                                : "Bạn bè"
+                                            : "Gửi kết bạn"}
+
+                                    </button>
+                                    <button className="message-btn" onClick={() => {navigate(`/messages/${userInfo.id}`)}}>Nhắn tin</button>
+                                </>
+                                )}
+                                
+                                {/* btn chỉnh sửa profile cho mình */}
+                                {isOwner && (
+                                    <button 
+                                        className="edit-profile-btn" 
+                                        onClick={() => handlingOpenEditProfileComponent()}>
+                                            Chỉnh sửa trang cá nhân
+                                    </button>
+                                )}
+                                
+                                <button className="more-btn">⋯</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="profile-stats">
-                        <span><strong>{userInfo.countPost}</strong> bài viết</span>
-                        <NavLink to="/friends" className="profile-stats__text">
-                            <strong>{userInfo.countFriend}</strong> bạn bè
-                        </NavLink>
-                    </div>
+                        <div className="profile-stats">
+                            <span><strong>{userInfo.countPost}</strong> bài viết</span>
+                            <NavLink to="/friends" className="profile-stats__text">
+                                <strong>{userInfo.countFriend}</strong> bạn bè
+                            </NavLink>
+                        </div>
 
-                    <div className="profile-bio">
-                        <p>{userInfo.bio}</p>
+                        <div className="profile-bio">
+                            <p>{userInfo.bio}</p>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            {showImageViewer && (
+                <ImageViewer
+                    isOwner={isOwner}
+                    images={userInfo.avatarUrl}
+                    onClose={() => setShowImageViewer(false)}
+                />
+            )}
         </div>
-    </div>
-  );
+    );
 }
 
 export default ProfileInfo;
